f29da7bfb637d0101167e949f78790c3867b5cca
geekbenny <117424402+geekbenny@users.noreply.github.com>
2025-04-28 21:18:58 +0300

    Update ussdroutes.js

--- routes/ussdroutes.js
+++ routes/ussdroutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { ussdHandler } = require('../controllers/ussdController');
+const ussdRoutes = require('./routes/ussdRoutes');
 
 router.post('/ussd', ussdHandler);
 
