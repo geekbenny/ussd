ecee80e4c299a295aeebf439ec2a1f58a1b128f3
geekbenny <117424402+geekbenny@users.noreply.github.com>
2025-04-29 00:20:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,12 @@ app.post('/admin/delete/:id', async (req, res) => {
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log('MongoDB connection failed:', err.message));
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
