2db5c3b19a6c4c018ac94f7a91a2a7c4b9ad8789
geekbenny <117424402+geekbenny@users.noreply.github.com>
2025-04-28 23:57:48 +0300

    Create Queue.js

---
+++ Queue.js
@@ -0,0 +1,29 @@
+// src/Queue.js
+
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(item) {
+    this.items.push(item);
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  peek() {
+    return this.items[0];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+}
+
+module.exports = Queue;
